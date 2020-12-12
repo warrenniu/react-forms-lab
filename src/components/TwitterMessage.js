5c865846787a940ef96749780b0d95faaad41559
warrenniu <warrenniu@gmail.com>
2020-12-12 17:05:15 -0500

    commit tree 1e36daa64a0021a18e9677ba06127d63840eafef

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -1,20 +1,32 @@
 import React from "react";
 
 class TwitterMessage extends React.Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
 
-    this.state = {};
+    console.log(props)
+    this.state = {
+      name: ""
+    };
+  }
+
+  changeHandler = (event) => {
+    console.log("changing event", event.target.value)
+    this.setState({name: event.target.value})
   }
 
   render() {
+    let charNumber = this.props.maxChars - this.state.name.length
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" value={this.state.name} onChange={this.changeHandler} />
+        <h5>{charNumber} characters left</h5>
       </div>
     );
   }
 }
 
 export default TwitterMessage;
+
+// Show the remaining characters in the component. It doesn't matter how you render it, as long as the number is correct. No need to guard against input that is too long — you can let the counter reach negative values.
